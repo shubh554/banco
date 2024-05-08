36f430c403d757af22b43a1fb6d96ab3ce0d3744
Shubhang Verma <shubhang554@gmail.com>
2024-05-08 19:50:02 +0530

    no use of kernal file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('app:verify-premium')->everyMinute();
-
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
