6ffd340d64f9c90c7a20d88aa694a6973bfbc9a8
Shubhang Verma <shubhang554@gmail.com>
2024-05-17 23:43:26 +0530

    comment added in console file

--- routes/console.php
+++ routes/console.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
+//verify commands
 Schedule::command('app:verify-premium')->everyMinute();
 Schedule::command('app:verify-staff')->everyMinute();
