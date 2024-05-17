843b4b90f55c7983ba75250b0f301ee06848483f
Shubhang Verma <shubhang554@gmail.com>
2024-05-17 21:37:42 +0530

    welcome message removed

--- app/Console/Commands/VerifyPremium.php
+++ app/Console/Commands/VerifyPremium.php
@@ -62,34 +62,6 @@ class VerifyPremium extends Command
                 if($response->status == 'valid')
                     {
                         Premium_Dealer_Contact::where('id', $id)->update(['verified' => 1]); 
-                        $url = 'https://api.ultramsg.com/instance85736/messages/chat';
-                        $params=array(
-                            'token' => 'o2uznzefj6qyd2oj',
-                            'to' => $mobile,
-                            'body'=>'Welcome to our WhatsApp Channel! 🎉 We are thrilled to have you onboard.Get ready for all the latest updates delivered straight to your fingertips.Have any questions or need assistance? Feel free to reach out to us directly through this channel. Lets stay connected!'
-                            );
-                            $curl = curl_init();
-                            curl_setopt_array($curl, array(
-                            CURLOPT_URL => $url,
-                            CURLOPT_RETURNTRANSFER => true,
-                            CURLOPT_ENCODING => "",
-                            CURLOPT_MAXREDIRS => 10,
-                            CURLOPT_TIMEOUT => 30,
-                            CURLOPT_SSL_VERIFYHOST => 0,
-                            CURLOPT_SSL_VERIFYPEER => 0,
-                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                            CURLOPT_CUSTOMREQUEST => "POST",
-                            CURLOPT_POSTFIELDS => http_build_query($params),
-                            CURLOPT_HTTPHEADER => array(
-                                "content-type: application/x-www-form-urlencoded"
-                            ),
-                            ));
-                        
-                            $response = curl_exec($curl);
-                            $err = curl_error($curl);
-                            
-                            curl_close($curl);
-                        
                     }
                     else
                     {
